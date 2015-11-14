746f74bd1b482257a9576434aa992d24929f7ae8
puwadon <puwadon.srit@gmail.com>
2015-11-14 16:02:06 +0700

    Finsish Day1

--- app/src/main/java/srit/puwadon/butraffic/MainActivity.java
+++ app/src/main/java/srit/puwadon/butraffic/MainActivity.java
@@ -1,6 +1,8 @@
 package srit.puwadon.butraffic;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -38,7 +40,9 @@ public class MainActivity extends AppCompatActivity {
                 buttonPlayer.start();
 
                 //Intent to WebView
-                
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                objIntent.setData(Uri.parse("https://www.facebook.com/Pawadon"));
+                startActivity(objIntent);
 
             } //Even
         });
