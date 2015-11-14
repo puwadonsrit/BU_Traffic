9586efca1a4cb15be61c1c63ef04980aa6983c4b
puwadon <puwadon.srit@gmail.com>
2015-11-14 15:00:38 +0700

    Sound Effect Successful

--- app/src/main/java/srit/puwadon/butraffic/MainActivity.java
+++ app/src/main/java/srit/puwadon/butraffic/MainActivity.java
@@ -1,13 +1,53 @@
 package srit.puwadon.butraffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit การประกาศตัวแปร
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget ผูกวิจเจต
+        bindWidget();
+
+        //Button Controller
+        buttonController();
+
+
+    } //Main method
+
+    private void buttonController() {
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Sound Effect
+                MediaPlayer buttonPlayer = MediaPlayer.create(getBaseContext(), R.raw.dog); //getBaseContext บอกว่าไฟล์อยู่ที่เดียวกับงาน
+                buttonPlayer.start();
+
+                //Intent to WebView
+                
+
+            } //Even
+        });
     }
-}
+
+    private void bindWidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+
+    }
+} //Main class คลาสหลัก
+
