5b9effee5036018e20e09c94f574b1bd575f9f89
puwadon <puwadon.srit@gmail.com>
2015-11-15 16:15:57 +0700

    day2 ยังไม่สมบูรณ์

--- app/src/main/java/srit/puwadon/butraffic/MainActivity.java
+++ app/src/main/java/srit/puwadon/butraffic/MainActivity.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -33,9 +34,13 @@ public class MainActivity extends AppCompatActivity {
 
     } //Main method
 
+    public void clickExercise(View view){
+        startActivity(new Intent(MainActivity.this, ExerciseActivity.class));
+    }
+
     private void createListview() {
         //Setup Array
-        String[] strTitle = new String[20];
+        final String[] strTitle = new String[20];
         strTitle[0] = "ห้ามเลี้ยวซ้าย";
         strTitle[1] = "ห้ามเลี้ยวขวา";
         strTitle[2] = "ตรงไป";
@@ -57,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
         strTitle[18] = "จำกัดความกว้าง";
         strTitle[19] = "จำกัดความสูง";
 
-        int[] intIcon = {R.drawable.traffic_01,R.drawable.traffic_02,
+        final int[] intIcon = {R.drawable.traffic_01,R.drawable.traffic_02,
                 R.drawable.traffic_03,R.drawable.traffic_04,R.drawable.traffic_05,
                 R.drawable.traffic_06,R.drawable.traffic_07,R.drawable.traffic_08,
                 R.drawable.traffic_09,R.drawable.traffic_10,R.drawable.traffic_11,
@@ -66,6 +71,17 @@ public class MainActivity extends AppCompatActivity {
                 R.drawable.traffic_18,R.drawable.traffic_19,R.drawable.traffic_20};
         MyAdapter objMyAdapter = new MyAdapter(MainActivity.this, strTitle, intIcon);
         trafficListView.setAdapter(objMyAdapter);
+
+        trafficListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent objIntent = new Intent(MainActivity.this, DetailActivity.class); //.this อยู่ที่ไหน .class ย้ายไปที่ไหน
+                objIntent.putExtra("Title", strTitle[position]);
+                objIntent.putExtra("Image", intIcon[position]);
+                objIntent.putExtra("Index", position);
+                startActivity(objIntent);
+            }// Event
+        });
 
 
     }// Create ListView
