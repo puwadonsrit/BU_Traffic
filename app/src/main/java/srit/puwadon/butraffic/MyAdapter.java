b2a60525bd851b64e6e3e045235c6b8865aeac2c
puwadon <puwadon.srit@gmail.com>
2015-11-15 11:13:03 +0700

    ListView Finish

--- app/src/main/java/srit/puwadon/butraffic/MyAdapter.java
+++ app/src/main/java/srit/puwadon/butraffic/MyAdapter.java
@@ -1,9 +1,15 @@
 package srit.puwadon.butraffic;
 
 import android.content.Context;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by student on 11/14/2015.
@@ -23,7 +29,7 @@ public class MyAdapter extends BaseAdapter{
 
     @Override
     public int getCount() {
-        return 0;
+        return titleStrings.length;
     }
 
     @Override
@@ -38,6 +44,16 @@ public class MyAdapter extends BaseAdapter{
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        LayoutInflater objLayoutInflater = (LayoutInflater) objContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View objView1 = objLayoutInflater.inflate(R.layout.my_listview, parent, false);
+
+        //Setup title
+        TextView titleTextView = (TextView) objView1.findViewById(R.id.textView2);
+        titleTextView.setText(titleStrings[position]);
+
+        //Setup Icon
+        ImageView iconImageView = (ImageView) objView1.findViewById(R.id.imageView);
+        iconImageView.setImageResource(iconInts[position]);
+        return objView1;
     }
 } // Main Class
